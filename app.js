cb91fc4ce00e9289c41f5c8a1694c9ec118006bd
layliskills <layliskills@gmail.com>
2026-03-05 19:55:58 +0300

    just created only adding buttom

---
+++ app.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", function() {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    list.appendChild(li);
+    input.value = "";
+});
